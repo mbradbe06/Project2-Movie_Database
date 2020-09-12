339f82ed2dc3c89a0b25f7895bdb64f36cb40670
trackrun82 <64228800+trackrun82@users.noreply.github.com>
2020-09-12 12:12:24 -0600

    updated html and javascript files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 #################################################
@@ -23,7 +23,7 @@ print(Base.classes.keys())
 # Save reference to the table
 Movie = Base.classes.movie
 Genre = Base.classes.genre
-mgjunct = Base.classes.movie_genre_junction
+# mgjunct = Base.classes.movie_genre_junction
 country = Base.classes.country_origin
 countryjunct = Base.classes.movie_country_junction
 companyname = Base.classes.production_company
@@ -43,14 +43,14 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def welcome():
-    return (
-        f"Welcome to our movie database API!<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/movies<br/>"
-        f"/api/v1.0/companies<br/>"
-        f"/api/v1.0/tbd<br/>"
-        f"/api/v1.0/tbd"
-    )
+    return render_template('index.html')
+        # f"Welcome to our movie database API!<br/>"
+        # f"Available Routes:<br/>"
+        # f"/api/v1.0/movies<br/>"
+        # f"/api/v1.0/companies<br/>"
+        # f"/api/v1.0/tbd<br/>"
+        # f"/api/v1.0/tbd"
+        
 
 @app.route("/api/v1.0/movies")
 def movie_info():
@@ -58,18 +58,19 @@ def movie_info():
                                Movie.movie_duration, Movie.budget, Movie.usa_gross_income,\
                                Movie.worlwide_gross_income,\
                                country.country_name, country.lat, country.long, Movie.votes_avg,\
-                               Genre.genre_name, companyname.company_name).filter(\
+                            #    Genre.genre_name,\
+                               companyname.company_name).filter(\
                                Movie.movie_id == countryjunct.movie_id).filter(\
                                Movie.company_id == companyname.company_id).filter(\
-                               Movie.movie_id == mgjunct.movie_id).filter(\
-                               mgjunct.genre_id == Genre.genre_id).filter(\
+                            #    Movie.movie_id == mgjunct.movie_id).filter(\
+                            #    mgjunct.genre_id == Genre.genre_id).filter(\
                                countryjunct.country_id == country.country_id).\
-                               limit(100).all()
+                               all()
 
     session.close()
     #Create list of movie information
     movie_list = []
-    for title, year, duration, budget, us, worldwide, country_name, lat, lng, avg_votes, genre, company in movie_info:
+    for title, year, duration, budget, us, worldwide, country_name, lat, lng, avg_votes, company in movie_info:
         movie_dict = {}
         movie_dict['title'] = title
         movie_dict['year_pub'] = year
@@ -81,7 +82,7 @@ def movie_info():
         movie_dict['lat'] = lat
         movie_dict['lng'] = lng
         movie_dict['avg_votes'] = avg_votes
-        movie_dict['genre'] = avg_votes
+        # movie_dict['genre'] = avg_votes
         movie_dict['company'] = company
         movie_list.append(movie_dict)
 
